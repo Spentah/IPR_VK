359965d7a1d6a9d26955441763723e6bf90c72ed
Kirill Botalov <cccombo.breaker.yo@gmail.com>
2021-11-15 01:48:58 +0400

    реализовал лайк, метод получения айди разделил на два разных

--- src/main/java/api/VkNewsFeed.java
+++ src/main/java/api/VkNewsFeed.java
@@ -18,7 +18,6 @@ public class VkNewsFeed {
 
     public void getRecommendedFeed() {
         recommendedFeed = given().spec(RequestSpecUtil.getSpecification()).log().all()
-//                .param("count", 10)
                 .get(EndPoints.GET_NEWSFEED.endPoint);
         recommendedFeed.then().statusCode(200);
 //        getIds(recommendedFeed);
@@ -29,7 +28,9 @@ public class VkNewsFeed {
 //        Integer itemId = (Integer) ids.get("item_id").get(postNumber - 1);
         Response response = given().spec(RequestSpecUtil.getSpecification()).log().all()
                   .param("type", "post")
-                  .params(getIdsFromRec(recommendedFeed, postNumber))
+                .param("owner_id", getOwnerIdByNum(recommendedFeed, postNumber))
+                .param("item_id", getItemIdByNum(recommendedFeed, postNumber))
+//                  .params(getIdsFromRec(recommendedFeed, postNumber))
 //                  .param("type", ids.get("type").get(postNumber - 1).toString())
 //                  .param("owner_id", ownerId.get(0))
 //                  .param("item_id", itemId)
@@ -46,9 +47,17 @@ public class VkNewsFeed {
         ids.put("owner_id", ownerIds);
     }
 
+    public int getOwnerIdByNum(Response response, int number) {
+        return JsonPath.from(response.asString()).getInt("response.items.attachments[" + number + "].photo.owner_id[0]");
+    }
+
+    public int getItemIdByNum(Response response,  int number) {
+        return JsonPath.from(response.asString()).getInt("response.items.post_id[" + number + "]");
+    }
+
     public Map<String, Integer> getIdsFromRec(Response response, int number) {
         int itemId = JsonPath.from(response.asString()).getInt("response.items.post_id[" + number + "]");
-        Integer ownerId = (Integer) JsonPath.from(response.asString()).getInt("response.items.attachments[" + number + "].photo.owner_id");
+        int ownerId = JsonPath.from(response.asString()).getInt("response.items.attachments[" + number + "].photo.owner_id[0]");
         Map<String, Integer> i = new HashMap<>();
         i.put("item_id", itemId);
         i.put("owner_id", ownerId);
